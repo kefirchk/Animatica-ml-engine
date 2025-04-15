4a0b9ba084f2d0dd045eec2043f053dc484991c2
kefirchk <prostolex2004@mail.ru>
2025-04-15 22:40:05 +0300

    Added main.py

--- app/demo.py
+++ app/demo.py
@@ -18,38 +18,6 @@ if sys.version_info[0] < 3:
 matplotlib.use("Agg")
 
 
-# def load_checkpoints(config_path, checkpoint_path, cpu=False):
-#     with open(config_path) as f:
-#         config = yaml.load(f, Loader=yaml.FullLoader)
-#
-#     generator = OcclusionAwareGenerator(
-#         **config["model_params"]["generator_params"], **config["model_params"]["common_params"]
-#     )
-#     if not cpu:
-#         generator.cuda()
-#
-#     kp_detector = KPDetector(**config["model_params"]["kp_detector_params"], **config["model_params"]["common_params"])
-#     if not cpu:
-#         kp_detector.cuda()
-#
-#     if cpu:
-#         checkpoint = torch.load(checkpoint_path, map_location=torch.device("cpu"))
-#     else:
-#         checkpoint = torch.load(checkpoint_path)
-#
-#     generator.load_state_dict(checkpoint["generator"])
-#     kp_detector.load_state_dict(checkpoint["kp_detector"])
-#
-#     if not cpu:
-#         generator = DataParallelWithCallback(generator)
-#         kp_detector = DataParallelWithCallback(kp_detector)
-#
-#     generator.eval()
-#     kp_detector.eval()
-#
-#     return generator, kp_detector
-
-
 def find_best_frame(source, driving, cpu=False):
     import face_alignment
 
